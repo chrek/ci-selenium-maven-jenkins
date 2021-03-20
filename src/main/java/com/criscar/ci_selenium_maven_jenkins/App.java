fe16f930191711923ab6568accda2eef432cf627
chrek <chrek@ymail.com>
2021-03-20 22:26:14 +0100

    first commit

---
+++ src/main/java/com/criscar/ci_selenium_maven_jenkins/App.java
@@ -0,0 +1,13 @@
+package com.criscar.ci_selenium_maven_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
